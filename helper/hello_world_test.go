7e7046267549860ce6e3f8f8b2f8d65c3c76541e
Yusuf Supriadi <yusufdolenk2@gmail.com>
2025-02-26 05:52:40 +0700

    feat: menggagalkan unit test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,19 +1,26 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Yusuf")
 	if result != "Hello Yusuf" {
-		// unit test failed
-		panic("Result is not Hello Yusuf")
+		// error
+		t.Error("Result must be Hello Yusuf")
 	}
+
+	fmt.Println("TestHelloWorld Done")
 }
 
 func TestHelloSupriadi(t *testing.T) {
 	result := HelloWorld("Supriadi")
 	if result != "Hello Supriadi" {
 		// unit test failed
-		panic("Result is not Hello Supriadi")
+		t.Fatal("Result must be Hello Supriadi")
 	}
+
+	fmt.Println("TestHelloSupriadi Done")
 }
